abab627dd4aca1089d109577e278853f9d4d67e5
William SZURKOWSKI <wszurkow@e3r2p7.clusters.42paris.fr>
2022-10-27 16:19:40 +0200

    added comments in server.cpp

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -18,7 +18,7 @@ Server::~Server(void) {}
 
 Server::Server(std::string passwd, std::string port) : _fdServer(-1), _passwd(passwd), _port(port) {
 	FD_ZERO(&(_set));
-	
+
 	memset(&_serverAddress, 0, sizeof(_serverAddress));
 	memset(&_clientAddress, 0, sizeof(_clientAddress));
 }
@@ -42,8 +42,8 @@ void	Server::setPort(std::string port) {
 int Server::newConnection() {
 	int fd;
 	socklen_t	addrlen = sizeof(_clientAddress);
-    struct epoll_event  ev;
-	
+	struct epoll_event  ev;
+
 	memset(&_clientAddress, 0, addrlen);
 	fd = accept(_fdServer, (struct sockaddr*)&_clientAddress, &addrlen);
 	if (_users.size() >= MAX_USERS) {
@@ -81,40 +81,86 @@ void	Server::requestClient(struct epoll_event user) {
 	buff[ret] = 0;
 
 	_buffUsers[user.data.fd].append(buff);
-	
+
 	// parsing a faire pour user
-	
+
 #if Debug
 	std::cout << _BLUE << buff << _NC;
 #endif
-	
+
 }
 
+
+/* ========================================================================== */
+/* ------------------------------ SERVER INIT ------------------------------- */
+/* ========================================================================== */
+
 void	Server::init(void) {
-	int opt = 1;
-	
-	if ((_fdServer = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+	/* .................................................. */
+	/* ................. SERVER SOCKET .................. */
+	/* .................................................. */
+	if ((_fdServer = socket
+				(
+				 AF_INET,     // AF_INET == PF_INET == 2 == protocol family (IPV4)
+				 SOCK_STREAM, // SOCK_STREAM means that it is a TCP socket. SOCK_DGRAM means that it is a UDP socket.
+				 0            //  Specifies a particular protocol to be used with the
+							  //  socket. Specifying a protocol of 0 causes socket() to
+							  //  use an unspecified default protocol appropriate for
+							  //  the requested socket type.
+				)
+		) < 0)
+	{
 		perror("Socket failed");
 		exit(EXIT_FAILURE);
 	}
-	
-	_serverAddress.sin_family = AF_INET;
-	_serverAddress.sin_addr.s_addr = INADDR_ANY;
-	_serverAddress.sin_port = htons(atoi(_port.c_str()));
-	
-	if (setsockopt(_fdServer, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+
+	/* .................................................. */
+	/* ............ SERVER SOCKET SRUCT INIT ............ */
+	/* .................................................. */
+	_serverAddress.sin_family      = AF_INET;                    // Address family - IPv4?
+	_serverAddress.sin_addr.s_addr = INADDR_ANY;                 // 32 bit IP address  - htonl, htons, ntohl, ntohs - convert values between host and network byte or
+	_serverAddress.sin_port        = htons(atoi(_port.c_str())); // 16 bit port number - htonl, htons, ntohl, ntohs - convert values between host and network byte or
+
+	// TODO wtf is going on here
+	int opt = 1; // TODO wtf is opt used for
+	if (setsockopt
+			(
+			 _fdServer,    // Server socket
+			 SOL_SOCKET,   // specifies the protocol level at which the option resides.
+						   // To set options at the socket level, specify the
+						   // level argument as SOL_SOCKET.
+						   // TODO [?] man says to use IPPROTO_TCP for TCP
+			 SO_REUSEADDR, // TODO Read this : https://stackoverflow.com/questions/3229860/what-is-the-meaning-of-so-reuseaddr-setsockopt-option-linux
+						   // The option_name argument specifies a single option to set. It can
+						   // be one of the socket-level options defined in sys_socket.h(0p)
+						   // SO_REUSEADDR Specifies that the rules used in validating addresses supplied to bind() should allow reuse of local addresses,
+						   // if this is supported by the protocol.
+						   // This option takes an int value. This is a Boolean option
+			 &opt,         // TODO [?] wtf (const_void * option_value)- what is the relation with SO_REUSEADDR
+			 sizeof(opt)
+			)
+	   )
+	{
 		perror("Setsockopt failed");
 		exit(EXIT_FAILURE);
 	}
-	
+
+	/* .................................................. */
+	/* ...................... BIND ...................... */
+	/* .................................................. */
+	// Bind need a general sockaddr, this is why we cast &sockaddr_in (which is spacialized) to sockaddr*
 	if ((bind(_fdServer, (struct sockaddr *)&_serverAddress, sizeof(_serverAddress))) < 0) {
 		perror("Bind failed");
 		exit(EXIT_FAILURE);
 	}
+
+	// Listen for socket connections and limit the queue of incoming connections
+	// Second parameter (int) is the maximum number of outstanding (pending) connections
 	if (listen(_fdServer, 5)) {
 		perror("Listen failed");
 		exit(EXIT_FAILURE);
 	}
+
 	// FD_SET(_fdServer, &(_set));
 }
 
@@ -129,6 +175,10 @@ void	Server::init(void) {
 //     __uint32_t events;    /* Événement epoll      */
 //     epoll_data_t data;    /* Variable utilisateur */
 // };
+
+/* ========================================================================== */
+/* ----------------------------- SERVER LAUNCH ------------------------------ */
+/* ========================================================================== */
 
 void	Server::launch(void) {
 	// const int 			MAX_FD = sysconf(_SC_OPEN_MAX); //necessaire pour un usage avec select
@@ -153,7 +203,7 @@ void	Server::launch(void) {
 		if ((ready = epoll_wait(_fdPoll, user_tmp, MAX_USERS, 0)) < 0) { // vois si le timeout on peut le mettre a 0
 			perror("Fail epoll wait");
 			exit(EXIT_FAILURE);
-			}
+		}
 		for (int i = 0 ; i < ready; i++) {
 			if (user[i].data.fd == _fdServer) {
 				newConnection();
@@ -173,6 +223,6 @@ void	Server::launch(void) {
 		else
 			std::cout << ".\n";
 	}
-	
+
 #endif
 }
