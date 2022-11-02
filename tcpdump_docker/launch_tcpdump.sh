15d4b6895e25d8d3f9696f7844314a490a763b97
wsz <wsz@arch.home>
2022-11-02 23:47:01 +0100

    ok

--- tcpdump_docker/launch_tcpdump.sh
+++ tcpdump_docker/launch_tcpdump.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 
+IMAGE_NAME="tcpdump_irc"
+CONTAINER_NAME=$IMAGE_NAME
 
 if [[ $1 == "build" ]]; then
-	docker build ./ -t td
+	docker build ./ -t $IMAGE_NAME
 fi
+
 
 docker images
 docker ps -a
-docker run -d -p 6667:6667 --name tcpdump td
-docker exec -it tcpdump tcpdump -Aqq port 6667
+docker run -d -p 6667:6667 --name $CONTAINER_NAME $CONTAINER_NAME
+docker exec -it $CONTAINER_NAME tcpdump -Aqq 6667
+
