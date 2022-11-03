6d0ce99da3f0b84dcf826c0f1d9e156ca30a4bc2
wsz <wsz@arch.home>
2022-11-03 17:35:19 +0100

    added admin stats trace wip

---
+++ srcs/commands/stats.cpp
@@ -0,0 +1,124 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stats.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 17:29:19 by wszurkow          #+#    #+#             */
+/*   Updated: 2022/11/03 17:33:23 by wszurkow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Server.hpp"
+
+void stats(Server *server, User user)
+{
+	if (check_ERR_NOSUCHSERVER(server, user))
+		return ;
+}
+
+// 4.3.2 Stats message
+
+// Command: STATS
+// Parameters: [<query> [<server>]]
+
+// The stats message is used to query statistics of certain server.  If
+// <server> parameter is omitted, only the end of stats reply is sent
+// back.  The implementation of this command is highly dependent on the
+// server which replies, although the server must be able to supply
+// information as described by the queries below (or similar).
+
+// A query may be given by any single letter which is only checked by
+// the destination server (if given as the <server> parameter) and is
+// otherwise passed on by intermediate servers, ignored and unaltered.
+// The following queries are those found in the current IRC
+// implementation and provide a large portion of the setup information
+// for that server.  Although these may not be supported in the same way
+// by other versions, all servers should be able to supply a valid reply
+// to a STATS query which is consistent with the reply formats currently
+// used and the purpose of the query.
+
+// The currently supported queries are:
+
+// c - returns a list of servers which the server may connect
+// to or allow connections from;
+// h - returns a list of servers which are either forced to be
+// treated as leaves or allowed to act as hubs;
+// i - returns a list of hosts which the server allows a client
+// to connect from;
+// k - returns a list of banned username/hostname combinations
+// for that server;
+// l - returns a list of the server's connections, showing how
+// long each connection has been established and the traffic
+// over that connection in bytes and messages for each
+// direction;
+// m - returns a list of commands supported by the server and
+// the usage count for each if the usage count is non zero;
+// o - returns a list of hosts from which normal clients may
+// become operators;
+// y - show Y (Class) lines from server's configuration file;
+// u - returns a string showing how long the server has been up.
+
+
+// Examples:
+
+// STATS m                         ; check the command usage for the server
+// you are connected to
+
+// :Wiz STATS c eff.org            ; request by WiZ for C/N line
+// information from server eff.org
+
+
+
+// Numeric Replies:
+
+// ERR_NOSUCHSERVER
+// RPL_STATSCLINE                  RPL_STATSNLINE
+// RPL_STATSILINE                  RPL_STATSKLINE
+// RPL_STATSQLINE                  RPL_STATSLLINE
+// RPL_STATSLINKINFO               RPL_STATSUPTIME
+// RPL_STATSCOMMANDS               RPL_STATSOLINE
+// RPL_STATSHLINE                  RPL_ENDOFSTATS
+
+
+//211     RPL_STATSLINKINFO
+//"<linkname> <sendq> <sent messages> \
+//<sent bytes> <received messages> \
+//<received bytes> <time open>"
+//
+//212     RPL_STATSCOMMANDS
+//"<command> <count>"
+
+//213     RPL_STATSCLINE
+//"C <host> * <name> <port> <class>"
+
+//214     RPL_STATSNLINE
+//"N <host> * <name> <port> <class>"
+
+//215     RPL_STATSILINE
+//"I <host> * <host> <port> <class>"
+
+//216     RPL_STATSKLINE
+//"K <host> * <username> <port> <class>"
+
+//218     RPL_STATSYLINE
+//"Y <class> <ping frequency> <connect \
+//frequency> <max sendq>"
+
+//219     RPL_ENDOFSTATS
+//"<stats letter> :End of /STATS report"
+
+//241     RPL_STATSLLINE
+//"L <hostmask> * <servername> <maxdepth>"
+
+//242     RPL_STATSUPTIME
+//":Server Up %d days %d:%02d:%02d"
+
+//243     RPL_STATSOLINE
+//"O <hostmask> * <name>"
+
+//244     RPL_STATSHLINE
+//"H <hostmask> * <servername>"
+
+
---
+++ srcs/commands/trace.cpp
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   trace.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 17:27:36 by wszurkow          #+#    #+#             */
+/*   Updated: 2022/11/03 17:34:48 by wszurkow         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Server.hpp"
+
+void trace(Server *server, User user)
+{
+	if (check_ERR_NOSUCHSERVER(server, user))
+		return ;
+}
+
+//Command: TRACE
+//Parameters: [<server>]
+
+//TRACE command is used to find the route to specific server.  Each
+//server that processes this message must tell the sender about it by
+//sending a reply indicating it is a pass-through link, forming a chain
+//of replies similar to that gained from using "traceroute".  After
+//sending this reply back, it must then send the TRACE message to the
+//next server until given server is reached.  If the <server> parameter
+//is omitted, it is recommended that TRACE command send a message to
+//the sender telling which servers the current server has direct
+//connection to.
+
+//If the destination given by "<server>" is an actual server, then the
+//destination server is required to report all servers and users which
+//are connected to it, although only operators are permitted to see
+//users present.  If the destination given by <server> is a nickname,
+//they only a reply for that nickname is given.
+
+
+//Examples:
+
+//TRACE *.oulu.fi                 ; TRACE to a server matching *.oulu.fi
+//:WiZ TRACE AngelDust            ; TRACE issued by WiZ to nick AngelDust
+
+
+//Numeric Replies:
+
+//ERR_NOSUCHSERVER
+
+//If the TRACE message is destined for another server, all intermediate
+//servers must return a RPL_TRACELINK reply to indicate that the TRACE
+//passed through it and where its going next.
+
+//RPL_TRACELINK
+//A TRACE reply may be composed of any number of the following numeric
+//replies.
+
+//RPL_TRACECONNECTING             RPL_TRACEHANDSHAKE
+//RPL_TRACEUNKNOWN                RPL_TRACEOPERATOR
+//RPL_TRACEUSER                   RPL_TRACESERVER
+//RPL_TRACESERVICE                RPL_TRACENEWTYPE
+//RPL_TRACECLASS
+//200     RPL_TRACELINK
+//
+//"Link <version & debug level> <destination> \
+//<next server>"
+
+//201     RPL_TRACECONNECTING
+//"Try. <class> <server>"
+
+//202     RPL_TRACEHANDSHAKE
+//"H.S. <class> <server>"
+
+//203     RPL_TRACEUNKNOWN
+//"???? <class> [<client IP address in dot form>]"
+
+//204     RPL_TRACEOPERATOR
+//"Oper <class> <nick>"
+
+//205     RPL_TRACEUSER
+//"User <class> <nick>"
+
+//206     RPL_TRACESERVER
+//"Serv <class> <int>S <int>C <server> \
+//<nick!user|*!*>@<host|server>"
+
+//208     RPL_TRACENEWTYPE
+//"<newtype> 0 <client name>"
+
+//261     RPL_TRACELOG
+//"File <logfile> <debug level>"
+
+//- The RPL_TRACE* are all returned by the server in
+//response to the TRACE message.  How many are
+//returned is dependent on the the TRACE message and
+
+
+//whether it was sent by an operator or not.  There
+//is no predefined order for which occurs first.
+//Replies RPL_TRACEUNKNOWN, RPL_TRACECONNECTING and
+//RPL_TRACEHANDSHAKE are all used for connections
+//which have not been fully established and are either
+//unknown, still attempting to connect or in the
+//process of completing the 'server handshake'.
+//RPL_TRACELINK is sent by any server which handles
+//a TRACE message and has to pass it on to another
+//server.  The list of RPL_TRACELINKs sent in
+//response to a TRACE command traversing the IRC
+//network should reflect the actual connectivity of
+//the servers themselves along that path.
+//RPL_TRACENEWTYPE is to be used for any connection
+//which does not fit in the other categories but is
+//being displayed anyway.
+
