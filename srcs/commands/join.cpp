5585d4acaa0c73d02024c836bb149b35b5538792
adlancel <adlancel@pop-os.localdomain>
2022-11-03 19:25:21 +0100

    join wip

--- srcs/commands/join.cpp
+++ srcs/commands/join.cpp
@@ -14,46 +14,59 @@
 
 void join(Server *serv, User user)
 {
-    (void) user;
-    std::vector<std::string> channels;
+  (void)user;
+  std::vector<std::string> channels;
+  switch (serv->_allBuff.size())
+  {
+  case 2:
+  {
     split(channels, serv->_allBuff[1], ",");
+    std::cout << "join called" << std::endl;
+    std::cout << channels.size() << std::endl;
     for (std::vector<std::string>::iterator it = channels.begin(); it != channels.end(); it++)
-        std::cout << "channels = " << *it << std::endl;
+      std::cout << "channels = " << *it << std::endl;
+    break;
+  }
+  default:
+    break;
+  }
 
-    // switch (serv->_allBuff.size())
-    // {
-    // case 1:
-    // {
-    //     std::string msg = NAME + ERR_NEEDMOREPARAMS(print_allBuff(serv->_allBuff));
-    //     send(user.getFd(), msg.c_str(), msg.length(), 0);
-    //     break;
-    // }
+  {
+  }
+  // switch (serv->_allBuff.size())
+  // {
+  // case 1:
+  // {
+  //     std::string msg = NAME + ERR_NEEDMOREPARAMS(print_allBuff(serv->_allBuff));
+  //     send(user.getFd(), msg.c_str(), msg.length(), 0);
+  //     break;
+  // }
 
-    // case 2:
-    // {
-    //     std::map<std::string, Channel>::iterator it = _channels.find(_allBuff[2]);
-    //     if (it != _channels.end())
-    //     {
-    //         // check if mdp
-    //         if ()
-    //         // check if invite only
-    //         // join existing channel;
-    //         break;
-    //     }
-    //     else
-    //     {
-    //     std::string msg = NAME + ERR_NOSUCHCHANNEL(print_allBuff(serv->_allBuff));
-    //     send(user.getFd(), msg.c_str(), msg.length(), 0);
+  // case 2:
+  // {
+  //     std::map<std::string, Channel>::iterator it = _channels.find(_allBuff[2]);
+  //     if (it != _channels.end())
+  //     {
+  //         // check if mdp
+  //         if ()
+  //         // check if invite only
+  //         // join existing channel;
+  //         break;
+  //     }
+  //     else
+  //     {
+  //     std::string msg = NAME + ERR_NOSUCHCHANNEL(print_allBuff(serv->_allBuff));
+  //     send(user.getFd(), msg.c_str(), msg.length(), 0);
 
-    //     }
-    // }
+  //     }
+  // }
 
-    // case 3:
-    //     break;
+  // case 3:
+  //     break;
 
-    // default:
-    //     break;
-    // }
-    // ERR_TOOMANYCHANNELS
+  // default:
+  //     break;
+  // }
+  // ERR_TOOMANYCHANNELS
   //  serv.createChannel()
 }
--- tcpdump_docker/launch_tcpdump.sh
+++ tcpdump_docker/launch_tcpdump.sh
@@ -2,11 +2,16 @@
 
 
 
+
+IMAGE_NAME="tcpdump_irc"
+CONTAINER_NAME=$IMAGE_NAME
+
 if [[ $1 == "build" ]]; then
-	docker build -t td ./
+  docker build ./ -t $IMAGE_NAME
 fi
+
 
 docker images
 docker ps -a
-docker run -d -p 6667:6667 --name tcpdump td
-docker exec -it tcpdump bash
+docker run -d -p 6667:6667 --name $CONTAINER_NAME $IMAGE_NAME
+docker exec -it $CONTAINER_NAME tcpdump -Aqq port 6667
