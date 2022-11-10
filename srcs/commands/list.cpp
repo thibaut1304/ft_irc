8cfa8e748dc2e5e01936d7ef01b557283d3c77dd
Adrien LANCELLE <adlancel@e2r3p12.clusters.42paris.fr>
2022-11-10 17:01:11 +0100

    list channel ok

--- srcs/commands/list.cpp
+++ srcs/commands/list.cpp
@@ -6,7 +6,7 @@
 /*   By: adlancel <adlancel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 15:00:39 by adlancel          #+#    #+#             */
-/*   Updated: 2022/11/10 15:40:27 by adlancel         ###   ########.fr       */
+/*   Updated: 2022/11/10 17:01:01 by adlancel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ void list(Server *serv, User user)
     std::map<std::string, Channel *> tmp = serv->getChannels();
     if (serv->_allBuff.size() == 1)
     {
-            std::string msg = RPL_LISTSTART(user.getNickname());
-            send(user.getFd(), msg.c_str(), msg.length(), 0);
+        std::cout << "call list with 1 arg" << std::endl;
+        std::string msg = RPL_LISTSTART(user.getNickname());
+        send(user.getFd(), msg.c_str(), msg.length(), 0);
         for (std::map<std::string, Channel *>::iterator it = tmp.begin(); it != tmp.end(); it++)
         {
             usernames.clear();
-            std::map<std::string, User *>::iterator it_user = it->second->getUsers().begin();
-            std::map<std::string, User *>::iterator it_user_end = it->second->getUsers().begin();
+            std::map<std::string, User *> tmp = it->second->getUsers();
+            std::map<std::string, User *>::iterator it_user = tmp.begin();
+            std::map<std::string, User *>::iterator it_user_end = tmp.end();
             while (it_user != it_user_end)
             {
-                usernames += " " + it_user->first;
+                usernames.append(" ").append(it_user->second->getUsername());
                 it_user++;
             }
             std::string msg = RPL_LIST(user.getNickname(), it->first, usernames, "");
